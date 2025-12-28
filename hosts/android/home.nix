bbbb1d3b85c9003c5ef5bc69d3b945e0e12d9426
PseudoFractal <kshitishkumarratha@gmail.com>
2025-12-28 11:06:40 +0530

    fix: user on adroid has to be 'nix-on-droid'

--- hosts/android/home.nix
+++ hosts/android/home.nix
@@ -19,18 +19,7 @@
 
   sops.age.keyFile = "/data/data/com.termux.nix/files/home/.config/sops/age/keys.txt";
 
-  programs.starship.settings.username = {
-    show_always = true;
-    format = "[$user]($style)@";
-    aliases = {"nix-on-droid" = "pseudofractal";};
-  };
-
-  programs.fish.interactiveShellInit = ''
-    set -gx USER pseudofractal
-  '';
-
   # OPTIMIZATIONS
-
   # Disable man-pages generation to speed up activation
   manual.manpages.enable = false;
   manual.html.enable = false;
